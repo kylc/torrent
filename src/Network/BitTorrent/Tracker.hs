27cc54076830c883a48db265660983c9d9a87a50
Kyle Cesare <kcesare@gmail.com>
2013-02-28 01:16:30 -0800

    Partial implementation of compact peer decoding.

--- src/Network/BitTorrent/Tracker.hs
+++ src/Network/BitTorrent/Tracker.hs
@@ -1,5 +1,18 @@
-module Network.BitTorrent.Tracker where
+module Network.BitTorrent.Tracker
+    ( TrackerRequest(..)
+    , TrackerResponse(..)
+    , Peer(..)
+    , Event(..)
+    , request
+    ) where
 
+import Data.Bits
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as B8
+import Data.Char (intToDigit, ord)
+import Data.List (intercalate)
+import qualified Data.Map as Map
+import Data.Word
 import Network.HTTP
 
 import Network.BitTorrent.Bencode
@@ -15,6 +28,7 @@ data TrackerRequest = TrackerRequest {
   , reqDownloaded :: Int
   , reqLeft :: Int
   , reqEvent :: Event
+  , reqCompact :: Int
   } deriving (Eq, Show)
 
 data TrackerResponse = TrackerResponse {
@@ -37,12 +51,22 @@ instance Show Event where
     show Stopped = "stopped"
     show Empty = "empty"
 
-request :: TrackerRequest -> IO TrackerResponse
+request :: TrackerRequest -> IO (Either String TrackerResponse)
 request req = do
-    rsp <- simpleHTTP $ getRequest url
-    return TrackerResponse { resInterval = 10
-                           , resPeers = []
-                           }
+    resp <- simpleHTTP $ getRequest url
+    body <- getResponseBody resp
+    code <- getResponseCode resp
+    case code of
+        (2, _, _) ->
+            case parseBencode body of
+                Left x -> return . Left . show $ x
+                Right bcode ->
+                    let (BDict dict) = bcode
+                        (BString cmp) = Map.findWithDefault (BString "") "peers" dict
+                    in  return . Right $ TrackerResponse { resInterval = 10
+                                                         , resPeers = decodePeers $ B8.pack cmp
+                                                         }
+        (_, _, _) -> return . Left $ "Unexpected tracker response code" ++ show code
   where
     url = reqAnnounce req ++ "?" ++ requestParams req
 
@@ -57,4 +81,18 @@ requestParams req = urlEncodeVars
     , ("downloaded", show $ reqDownloaded req)
     , ("left", show $ reqLeft req)
     , ("event", show $ reqEvent req)
+    , ("compact", show $ reqCompact req)
     ]
+
+decodePeers :: B.ByteString -> [Peer]
+decodePeers bs | B.length bs < 6 = []
+               | otherwise =
+                   let (ip, r0) = B.splitAt 4 bs
+                       (port, r1) = B.splitAt 2 r0
+                       ip' = intercalate "." $ map show $ B.unpack ip
+                       port' = roll $ B.unpack port
+                   in  Peer "test123" ip' port' : decodePeers r1
+
+roll :: [Word8] -> Int
+roll = foldr unstep 0
+  where unstep b a = a `shiftL` 8 .|. fromIntegral b
