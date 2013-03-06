7972b3d4d2781880a5ceadfdfd818903fb774dd4
Kyle Cesare <kcesare@gmail.com>
2013-03-06 01:40:23 -0800

    Extract peer connection into function.

--- src/BitTorrent/PeerManager.hs
+++ src/BitTorrent/PeerManager.hs
@@ -2,6 +2,7 @@ module BitTorrent.PeerManager where
 
 import Control.Concurrent
 import Control.Monad
+import Data.Word
 import Network
 import Network.Socket
 
@@ -21,13 +22,18 @@ runPeer :: Peer -> IO ()
 runPeer p = do
     putStrLn $ "Connecting to " ++ show p
 
-    let addr = SockAddrInet (PortNum . fromIntegral $ peerPort p) (peerIp p)
-    sock <- socket AF_INET Stream defaultProtocol
-    connect sock addr
+    sock <- peerConnect (peerIp p) (peerPort p)
     handshake sock
     str <- recv sock 1
 
     return ()
+
+peerConnect :: Word32 -> Word16 -> IO Socket
+peerConnect a p = do
+    let addr = SockAddrInet (PortNum . fromIntegral $ p) a
+    sock <- socket AF_INET Stream defaultProtocol
+    connect sock addr
+    return sock
 
 handshake :: Socket -> IO Int
 handshake s = send s "19BitTorrent protocol00000000123456789123456789012345678901234567890"
