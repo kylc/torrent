406e55fd8abaf0ff06a5321d2724a5aa1d92e91f
Kyle Cesare <kcesare@gmail.com>
2013-03-06 01:49:12 -0800

    Formatting fixes.

--- src/BitTorrent/Types.hs
+++ src/BitTorrent/Types.hs
@@ -4,38 +4,38 @@ import Data.Word
 
 type Hash = String
 
-data Metainfo = Metainfo {
-    mtAnnounce :: String
-  , mtInfoHash :: String
-  , mtName :: String
-  , mtPieceLen :: Integer
-  , mtPieces :: [Hash]
-  , mtLength :: Maybe Integer
-  , mtFiles :: Maybe [MetainfoFile]
-  }
+data Metainfo = Metainfo
+    { mtAnnounce :: String
+    , mtInfoHash :: String
+    , mtName :: String
+    , mtPieceLen :: Integer
+    , mtPieces :: [Hash]
+    , mtLength :: Maybe Integer
+    , mtFiles :: Maybe [MetainfoFile]
+    }
 
-data MetainfoFile = MetainfoFile {
-    mtfLength :: Integer
-  , mtfPath :: String
-  }
+data MetainfoFile = MetainfoFile
+    { mtfLength :: Integer
+    , mtfPath :: String
+    }
 
-data TrackerRequest = TrackerRequest {
-    reqAnnounce :: String
-  , reqInfoHash :: String
-  , reqPeerId :: String
-  , reqIp :: String
-  , reqPort :: Int
-  , reqUploaded :: Int
-  , reqDownloaded :: Int
-  , reqLeft :: Int
-  , reqEvent :: Event
-  , reqCompact :: Int
-  } deriving (Eq, Show)
+data TrackerRequest = TrackerRequest
+    { reqAnnounce :: String
+    , reqInfoHash :: String
+    , reqPeerId :: String
+    , reqIp :: String
+    , reqPort :: Int
+    , reqUploaded :: Int
+    , reqDownloaded :: Int
+    , reqLeft :: Int
+    , reqEvent :: Event
+    , reqCompact :: Int
+    } deriving (Eq, Show)
 
-data TrackerResponse = TrackerResponse {
-    resInterval :: Int
-  , resPeers :: [Peer]
-  } deriving (Eq, Show)
+data TrackerResponse = TrackerResponse
+    { resInterval :: Int
+    , resPeers :: [Peer]
+    } deriving (Eq, Show)
 
 data Event = Started | Completed | Stopped | Empty
     deriving (Eq)
@@ -46,11 +46,11 @@ instance Show Event where
     show Stopped = "stopped"
     show Empty = "empty"
 
-data Peer = Peer {
-    peerId :: String
-  , peerIp :: Word32
-  , peerPort :: Word16
-  } deriving (Eq, Show)
+data Peer = Peer
+    { peerId :: String
+    , peerIp :: Word32
+    , peerPort :: Word16
+    } deriving (Eq, Show)
 
 -- TODO: Fill these in
 data Message = Choke
