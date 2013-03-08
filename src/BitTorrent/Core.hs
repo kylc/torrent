f30d5ff8895c6b8cace9787ca3c065017448680e
Kyle Cesare <kcesare@gmail.com>
2013-03-07 23:36:27 -0800

    Formatting.

--- src/BitTorrent/Core.hs
+++ src/BitTorrent/Core.hs
@@ -15,6 +15,7 @@ run :: String -> IO ()
 run f = do
     -- Read and decode the .torrent file
     b <- parseBencodeFile f
+
     -- Request tracker information
     let metainfo = fromJust $ readMetainfo b
         req = TrackerRequest { reqAnnounce = mtAnnounce metainfo
@@ -29,11 +30,11 @@ run f = do
                              , reqCompact = 1
                              }
     resp <- request req
+
+    -- Connect to peers
     case resp of
         Right resp -> forkIO . runPeerMgr metainfo $ resPeers resp
         Left e -> fail $ "Failed to parse bencode file: " ++ e
-
-    -- Connect to peers
 
     -- Download!
     forever $ threadDelay $ 1000000 * 1 -- 1 second
